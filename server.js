2024b2465132fe8f28a7bd28ba0328dcb75bd1de
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2022-11-19 13:27:51 +0300

    move utils to the right dir

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ const app = express();
 dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {origin:"http://localhost:3000"}
+));
 
 // endpoints
 
